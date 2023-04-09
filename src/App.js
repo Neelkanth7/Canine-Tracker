a395eaa8461d5408a6dd9cc2d5dcd7d2919e7d56
neelkanthtripathi <ntripat2@ncsu.edu>
2023-04-09 14:46:48 -0400

    Imlement Weather APi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from 'react';
+
 import {Typography} from '@material-ui/core'; 
 import NavBar from './components/NavBar'
 import Grid from './components/Grid'
 import Footer from './components/Footer'
 import './App.css';
 //changes to imports 
-import SecurityIcon from '@material-ui/icons/Security';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import TrendingUpIcon from '@material-ui/icons/TrendingUp';
-import ImportExportIcon from '@material-ui/icons/ImportExport';
-import ComputerIcon from '@material-ui/icons/Computer';
-import HttpIcon from '@material-ui/icons/Http';
+import DoorBackSharpIcon from '@mui/icons-material/DoorBackSharp';
+import PetsSharpIcon from '@mui/icons-material/PetsSharp';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
 
 const theme = createMuiTheme({
   palette: {
@@ -59,28 +59,24 @@ const styles = makeStyles({
 
 function App() {
   const classes = styles(); 
-
+  
+    
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <NavBar/>
         <div className={classes.wrapper}>
           <Typography variant="h4" className={classes.bigSpace} color="primary">
-             At Rocket.io we are passionate about software
+             At CANINE OUTDOOR ACTIVITY TRACKER we are passionate about your dog.
           </Typography>
           <Typography variant="h5" className={classes.littleSpace} color="primary">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales congue tristique. Cras non pretium sem. Duis interdum lorem sit amet ligula pretium, sed rutrum urna semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus egestas gravida ullamcorper.
-          </Typography>
+          The proposed system aims to provide dog owners with useful information about their dog's outdoor activities. The system will track the time of day and length of time their dog is outside and provide activity estimates based on movement for each outdoor session. It will also monitor the weather forecast and suggest if the dog should go outside soon, and keep records of the average time outside based on the time of day and weather conditions. Additionally, the system will send a text notification to the owner when their dog is ready to come inside. Overall, the system will provide a comprehensive monitoring solution to dog owners, enabling them to stay informed and make informed decisions about their pet's outdoor activities.          </Typography>
         </div>
         <div className={`${classes.grid} ${classes.bigSpace}`}>
-          <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
-          <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
-          <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
-        </div>
-        <div className={`${classes.grid} ${classes.littleSpace}`}>  
-          <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
-          <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
-          <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
+          <Grid icon={<WbSunnyIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Weather" />
+          
+          <Grid icon={<PetsSharpIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Steps" btnTitle="Show me More"/>
+          <Grid icon={<DoorBackSharpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Door Status" btnTitle="Show me More"/>
         </div>
         <div className={classes.bigSpace}>
           <Footer/>
